6b74de57ce835a07ac737ebc2843b27379589bba
Hugo Oliveira Soares <33163249+HugoOliveiraSoares@users.noreply.github.com>
2017-11-03 12:57:58 -0200

    Update emissor.ino

--- emissor.ino
+++ emissor.ino
@@ -1,4 +1,5 @@
 #define tam 8
+
 const int ledPin = 9;
 char mensagem;
 int ascii;
@@ -16,55 +17,56 @@ void loop()
 {
   if(Serial.available())
   {
-      for(int i=0; i<tam; i++)
+    for(int i=0; i<tam; i++)
+    {
+      mensagem = Serial.readString();
+      ascii =(int) mensagem;
+      if (ascii >= 1)
       {
-           mensagem = Serial.readString();
-           ascii =(int) mensagem;
-           if (ascii >= 1)
-            {
-              while (ascii/2 > 0)
-              {
-                bin[i] = ascii%2;
-                ascii /= 2;
-                i--;
-                j--;
-              }
+        while (ascii/2 > 0)
+        {
+          bin[i] = ascii%2;
+          ascii /= 2;
+          i--;
+          j--;
+        }
 
-              for (i = 0; i <j; i++)
-                bin[i] = 0;
+        for (i = 0; i <j; i++)
+          bin[i] = 0;
 
-              bin[j] = 1;
+        bin[j] = 1;
 
-              Serial.print("Binario:");
-              for (i = 0; i < tam; i++)
-              {
-                Serial.print("%2i",bin[i]);
-              }
+        Serial.print("Binario:");
+        for (i = 0; i < tam; i++)
+        {
+          Serial.print("%2i",bin[i]);
+        }
 
-            } else if (ascii == 0)
-            {
-              for (i = 0; i <tam; i++)
-              {
-                bin[i] = 0;
-              }
+      } 
+      else if (ascii == 0)
+      {
+        for (i = 0; i <tam; i++)
+        {
+          bin[i] = 0;
+        }
 
-              Serial.print("Binario:");
-              for (i = 0; i <tam; i++)
-              {
-                Serial.print("%2i",bin[i]);
-              }
+        Serial.print("Binario:");
+        for (i = 0; i <tam; i++)
+        {
+          Serial.print("%2i",bin[i]);
+        }
 
-            }
+      }
 
-            Serial.print("\n\n");
-          }
-           
-    
+      Serial.print("\n\n");
+    }
+
+
   }
   else
   {
     digitalWrite(ledPin,HIGH);
   }
-  
+
 }// end loop
 
