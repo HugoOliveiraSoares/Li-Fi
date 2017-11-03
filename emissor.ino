88a8cd059b88b0a14d52c5fe8c994c1a08cddf95
Hugo Oliveira Soares <33163249+HugoOliveiraSoares@users.noreply.github.com>
2017-11-03 12:55:56 -0200

    Update emissor.ino

--- emissor.ino
+++ emissor.ino
@@ -1,5 +1,10 @@
+#define tam 8
 const int ledPin = 9;
-String mensagen = "00000000";
+char mensagem;
+int ascii;
+int bin[tam];
+int i = 7;
+int j = i;
 
 void setup() 
 {
@@ -11,7 +16,50 @@ void loop()
 {
   if(Serial.available())
   {
-    mensagem = Serial.readString();
+      for(int i=0; i<tam; i++)
+      {
+           mensagem = Serial.readString();
+           ascii =(int) mensagem;
+           if (ascii >= 1)
+            {
+              while (ascii/2 > 0)
+              {
+                bin[i] = ascii%2;
+                ascii /= 2;
+                i--;
+                j--;
+              }
+
+              for (i = 0; i <j; i++)
+                bin[i] = 0;
+
+              bin[j] = 1;
+
+              Serial.print("Binario:");
+              for (i = 0; i < tam; i++)
+              {
+                Serial.print("%2i",bin[i]);
+              }
+
+            } else if (ascii == 0)
+            {
+              for (i = 0; i <tam; i++)
+              {
+                bin[i] = 0;
+              }
+
+              Serial.print("Binario:");
+              for (i = 0; i <tam; i++)
+              {
+                Serial.print("%2i",bin[i]);
+              }
+
+            }
+
+            Serial.print("\n\n");
+          }
+           
+    
   }
   else
   {
@@ -19,3 +67,4 @@ void loop()
   }
   
 }// end loop
+
