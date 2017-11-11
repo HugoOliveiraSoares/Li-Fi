abe9176b5cc8226350527325a6d015094c83f07b
Hugo Oliveira Soares <33163249+HugoOliveiraSoares@users.noreply.github.com>
2017-11-11 09:51:15 -0200

    Update emissor.ino

--- emissor.ino
+++ emissor.ino
@@ -1,71 +1,95 @@
 #define tam 8
 
-const int ledPin = 9;
+const int led = 13;
 char mensagem;
 int ascii;
 int bin[tam];
 int i = 7;
 int j = i;
+int *p;
+
+void conversor();
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(led, OUTPUT);
 }// end setup
 
 void loop() 
 {
   if(Serial.available())
   {
-    for(int i=0; i<tam; i++)
+    mensagem = Serial.read();
+
+    conversor();
+
+    p = bin;
+
+    for(int x= 0; x< tam; x++)
     {
-      mensagem = Serial.read();
-      ascii =(int) mensagem;
-      if (ascii >= 1)
+      if( *(p+x) == 1)
       {
-        while (ascii/2 > 0)
-        {
-          bin[i] = ascii%2;
-          ascii /= 2;
-          i--;
-          j--;
-        }
-
-        for (i = 0; i <j; i++)
-          bin[i] = 0;
-
-        bin[j] = 1;
-
-        Serial.print("Binario:");
-        for (i = 0; i < tam; i++)
-        {
-          Serial.print("%2i");
-          Serial.print(bin[i]);
-        }
-
-      } 
-      else if (ascii == 0)
-      {
-        for (i = 0; i <tam; i++)
-        {
-          bin[i] = 0;
-        }
-
-        Serial.print("Binario:");
-        for (i = 0; i <tam; i++)
-        {
-          Serial.print("%2i");
-          Serial.print(bin[i]);
-        }
-
+        digitalWrite(led, HIGH);
+        //Serial.print("ligado ");
       }
+      else
+      {
+        digitalWrite(led, LOW); 
+        //)Serial.print("desligado ");
+      }
+         delay(1000);
     }
-
-
   }
   else
   {
-    digitalWrite(ledPin,HIGH);
+    digitalWrite(led,LOW);
   }
 
+
 }// end loop
+
+void conversor()
+{
+  ascii =(int) mensagem;
+  Serial.print("ascii");
+  Serial.println(ascii);
+  if (ascii >= 1)
+  {
+    while (ascii/2 > 0)
+    {
+      bin[i] = ascii%2;
+      ascii /= 2;
+      i--;
+      j--;
+    }
+
+    for (i = 0; i <j; i++)
+      bin[i] = 0;
+
+    bin[j] = 1;
+
+    Serial.print("Binario:");
+    for (i = 0; i < tam; i++)
+    {
+      Serial.print(bin[i]);
+    }
+
+  } 
+  else if (ascii == 0)
+  {
+    for (i = 0; i < tam; i++)
+    {
+      bin[i] = 0;
+    }
+
+    Serial.print("Binario:");
+    for (i = 0; i < tam; i++)
+    {
+      Serial.print(bin[i]);
+    }
+
+  }
+  Serial.print("\n\n");
+}
+
